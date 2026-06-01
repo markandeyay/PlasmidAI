f9fca0586d5d18113524b4942fefec8028588779
Markandeya Yalamanchi <markandeyayalamanchi9@gmail.com>
2026-06-01 18:04:40 -0400

    Add deterministic SequenceGenerator scaffold

---
+++ packages/generation/__init__.py
@@ -0,0 +1,8 @@
+from .generator import FAKE_GENERATOR_VERSION, FakeGenerator, MarkerSwap, SequenceGenerator
+
+__all__ = [
+    "FAKE_GENERATOR_VERSION",
+    "FakeGenerator",
+    "MarkerSwap",
+    "SequenceGenerator",
+]
